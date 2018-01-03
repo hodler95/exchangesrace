37cef19c897ae52ddf23a42dc7b65b8c0c50af67
Adriano Ferraguto <0xADFE@gmail.com>
2018-01-03 17:54:31 +0100

    little adjustments

--- tracker.js
+++ tracker.js
@@ -3,20 +3,19 @@ var BtczBalance2 = 0;
 var BtczValue = 0;
 
 function updatePage(){
-    document.getElementById('btczBalance1').innerHTML = BtczBalance1.toFixed(0) + " BTCZ";
-    document.getElementById('btczBalance2').innerHTML = BtczBalance2.toFixed(0) + " BTCZ";
     var BtczBalance1_toBtc = (BtczBalance1*BtczValue);
     var BtczBalance2_toBtc = (BtczBalance2*BtczValue);
+    document.getElementById('btczBalance1').innerHTML = BtczBalance1.toFixed(0) + " BTCZ";
+    document.getElementById('btczBalance2').innerHTML = BtczBalance2.toFixed(0) + " BTCZ";
     document.getElementById('btczBalance1_toBtc').innerHTML = "<b>" + BtczBalance1_toBtc.toFixed(8) + " BTC</b>";
     document.getElementById('btczBalance2_toBtc').innerHTML = "<b>" + BtczBalance2_toBtc.toFixed(8) + " BTC</b>";
-    document.getElementById('progressBar').style.width = (BtczBalance1_toBtc/2.50).toFixed(2) + "%";
-    document.getElementById('progressPercent1').innerHTML = (BtczBalance1_toBtc/2.50).toFixed(0) + "%";
-    document.getElementById('progressBar2').style.width = (BtczBalance2_toBtc/2.50).toFixed(2) + "%";
-    document.getElementById('progressPercent2').innerHTML = (BtczBalance2_toBtc/2.50).toFixed(0) + "%";
+    document.getElementById('progressBar').style.width = (BtczBalance1_toBtc/0.025).toFixed(2) + "%";
+    document.getElementById('progressPercent1').innerHTML = (BtczBalance1_toBtc/0.025).toFixed(0) + "%";
+    document.getElementById('progressBar2').style.width = (BtczBalance2_toBtc/0.025).toFixed(2) + "%";
+    document.getElementById('progressPercent2').innerHTML = (BtczBalance2_toBtc/0.025).toFixed(0) + "%";
 }
 
 function getBalances(){
-    console.log("function getBalances() triggered");
     var xhr1 = new XMLHttpRequest();
     xhr1.open("GET", "https://bitcoinz.ph/api/addr/t1L2h7WyVsxEvbw1acefwXWBQWYReQnN7MF/balance", true);
     xhr1.onload = function(){
@@ -50,7 +49,6 @@ function getBalances(){
 }
 
 function start(){
-    console.log("function start() triggered");
     getBalances();
     setInterval(function(){getBalances();}, 300000);
 }
